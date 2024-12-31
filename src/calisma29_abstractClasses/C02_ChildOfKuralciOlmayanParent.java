c13ba671aa63402f53498846990e15f3a33004d8
somkanserat <serat.somkan@hotmail.com>
2024-12-31 12:25:05 +0300

    twelfth commit

---
+++ src/calisma29_abstractClasses/C02_ChildOfKuralciOlmayanParent.java
@@ -0,0 +1,27 @@
+package calisma29_abstractClasses;
+
+public class C02_ChildOfKuralciOlmayanParent extends C01_KuralciOlmayanParent{
+
+    public static void main(String[] args) {
+
+        /*
+            Java inheritance'da KRAL child class'dir
+
+            - child class istedigi parent'i secebilir
+            - child class, parent sectigi class'dan
+               istedigi ozellikleri hic degistirmeden kullanabilir
+               istedigi ozellikleri kendine uyarlar ( implement )
+               isterse de parent class'da olmayan yeni ozellikler ekleyebilir
+
+            Klasik inheritance'da
+            parent class'larin child class'lar uzerinde
+            hic bir zorlama hakki yok
+
+            EGER bir class
+            kendini parent edinecek child class'larin
+            MUTLAKA bulundurmasi gereken method'lari belirlemek isterse
+            ABSTRACTION kullanilir
+         */
+
+    }
+}
---
+++ src/calisma29_abstractClasses/C06_Araba.java
@@ -0,0 +1,19 @@
+package calisma29_abstractClasses;
+
+public abstract class C06_Araba {
+
+    public abstract void motor();
+    public abstract void sase();
+    public abstract void sanzuman();
+    public abstract void fren();
+    public abstract void guvenlik();
+
+    // public void renk();
+    // Missing method body, or declare abstract
+    // abstract diye tanimla veya body olustur
+
+//    public abstract void renk(){
+//        // Abstract methods cannot have a body
+//    }
+
+}
