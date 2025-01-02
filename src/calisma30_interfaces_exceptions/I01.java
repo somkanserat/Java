2f3b9eedfda66bc31ed2615ca3a55834a1c4433e
somkanserat <serat.somkan@hotmail.com>
2025-01-02 12:59:18 +0300

    thirteenth commit

---
+++ src/calisma30_interfaces_exceptions/I01.java
@@ -0,0 +1,44 @@
+package calisma30_interfaces_exceptions;
+
+public interface I01 {
+
+    String str = "I01 str";
+    int sayi = 45;
+    boolean izindeMi = true;
+
+    // void method1(){};
+    // Interface abstract methods cannot have body
+
+    void method1();
+
+    void method3();
+
+    //void method4();
+
+    // void method7();
+    // Bir interface'e sonradan bir abstract method eklediginizde
+    // o interface'i implement etmis olan TUM CHILD CLASS"lar bundan etkilenir
+    // tum child class'larin bu yeni method'u kendisine uyarlamasi gerekir
+    // bu da bitmis bir proje de negatif etkiler olusturabilir
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
