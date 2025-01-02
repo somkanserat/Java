4c5a8a33fed114bde60d0f0caf5c791de0bebdef
somkanserat <serat.somkan@hotmail.com>
2025-01-02 12:58:01 +0300

    thirteenth commit

---
+++ src/calisma31_exceptions/C06_SayiToplama.java
@@ -0,0 +1,47 @@
+package calisma31_exceptions;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class C06_SayiToplama {
+
+    public static void main(String[] args) {
+
+        // Kullanicidan toplanmak uzere tamsayilar isteyin
+        // ve kullanici q veya Q'ya basincaya kadar islemi devam ettirin
+        // Kullanici q'ya bastiginda
+        // kac adet sayi girildigini ve toplamin ne oldugunu yazdirin
+
+
+        Scanner scanner = new Scanner(System.in);
+        int toplam = 0;
+        int sayac = 0;
+        int girilenSayi = 0;
+        String girilenMetin = "";
+
+
+        do{
+
+            System.out.println("Lutfen toplanmak uzere tamsayi girin,\nbitirmek icin Q'ya basin");
+
+
+            try {
+                girilenSayi = scanner.nextInt();
+                toplam += girilenSayi;
+                sayac++;
+
+            } catch (InputMismatchException e) {
+                girilenMetin = scanner.nextLine(); // q , sanane, 4.5, true, Quinn .......
+
+                if (girilenMetin.equalsIgnoreCase("Q")){
+                    break;
+                }else System.out.println("GIRIS HATALI... tamsayi veya Q olmali");
+            }
+
+
+        }while ( !girilenMetin.equalsIgnoreCase("q"));
+
+        System.out.println("Girilen " + sayac + " adet sayinin toplami : " + toplam);
+
+    }
+}
---
+++ src/calisma31_exceptions/C08_MultipleCheckedExceptions.java
@@ -0,0 +1,54 @@
+package calisma31_exceptions;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class C08_MultipleCheckedExceptions {
+
+    public static void main(String[] args) {
+
+        /*
+            FileInputStream fileInputStream olusturdugumuzda
+            Java Ya dosyayi bulamazsam dedi,
+            biz de try catch ile sarmalayip
+            FileNotFoundException'i kontrol altina aldik
+
+            Ancak fileInputStream.read() yazdigimizda
+            bu defa da IOException riski barindirdigi icin altini kirmizi cizdi
+
+            IOException, FileNotFoundException'in parent'i oldugundan
+            islem yaparken bu iliskiyi dikkate almaliyiz
+            1- eger ikisini de ayri catch cumleleri ile yazacaksak
+               uste child exception yazilmalidir
+
+            2- eger ikisini ayri ayri yazmak istemezsek
+               sadece parent olan IO exception'i yazmamiz yeterli olacaktir
+               cunku IOException ikisini de kapsar
+
+         */
+
+        // notlar dosyasindaki yazilari konsolda yazdirin
+
+        String dosyaYolu = "src/day31_exceptions/notlar.txt";// notlar-->sağ tuş+copy path/reference-->src/... olan seçilir.
+        FileInputStream fileInputStream;
+
+        try {
+            fileInputStream = new FileInputStream(dosyaYolu);
+            // dosyadaki yazilari konsolda yazdirin
+
+            int k=0;
+            while((k= fileInputStream.read() ) != (-1)) { // dosyayi okuyup, bize getirme
+                // Unhandled exception: java.io.IOException
+                System.out.print((char) k);
+            }
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Verilen dosya yolu hatali...");
+
+        } catch (IOException e) { // IO ==> Input Output // bu metot olmasaydı, read'in altı kırmızı yanardı
+                                                         // read'in üstüne gelip, add catch clause dedik.
+            System.out.println("Dosyadaki degerler okunamiyor");
+        }
+    }
+}
